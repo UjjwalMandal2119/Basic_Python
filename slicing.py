17fd280d775e2b2846247686fd858d4f122258c0
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-07-07 08:44:56 +0530

    learning slicing

---
+++ slicing.py
@@ -0,0 +1,8 @@
+example=[1,2,3,4,5,6,7,8,9,10]
+print(example[2:5])
+print(example[:5])
+print(example[5:10])
+print(example[::2])
+print(example[1:10:2])
+print(example[::-1])
+print(example[5:1:-1])
