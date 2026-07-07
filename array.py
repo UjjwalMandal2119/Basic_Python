2796ae86c621f388320abc36431e964fefabaa5c
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-07-07 08:41:57 +0530

    learning numpy

--- array.py
+++ array.py
@@ -1,4 +1,11 @@
-import array as arr
-my_arr=arr.array('i',[1,2,3,4,5])
+# import array as arr
+# my_arr=arr.array('i',[1,2,3,4,5])
+# print(my_arr)
+# print(type(my_arr))
+
+import numpy as np
+my_arr=np.array(["hello",1,2,3,4,5])
 print(my_arr)
 print(type(my_arr))
+# import numpy as np
+# print(np.__version__)
