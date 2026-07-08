43550e8dccdf6000a0dad437c4e3b7d9b72422d6
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-07-08 23:58:46 +0530

    learning python

---
+++ add.py
@@ -0,0 +1,13 @@
+num1= int(input("Enter first number: "))
+num2= int(input("Enter second number: "))
+sum= num1 + num2
+print("The sum is: ", sum)
+
+
+Notes:
+input()
+
+# Always returns a string.
+# Convert the string into an integer.
+# int(input())
+# Tip: Avoid using sum as a variable name in real projects because sum() is also a built-in Python function. A better name is total.
--- input.py
+++ input.py
@@ -1,2 +1,5 @@
-name = input("Enter your name : ")
+name = input("Enter your name : ")  
+# input() takes input from the keyboard.
+# The text inside parentheses is called the prompt.
+#  # The entered value is stored in the variable name.
 print("Hello, " + name + "!")
