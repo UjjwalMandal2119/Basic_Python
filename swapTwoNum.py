d830544305c4cb979976d5e2c19d0493d7ec1c34
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-07-09 23:53:00 +0530

    swap two number

---
+++ swapTwoNum.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+temp=num1
+num1=num2
+num2=temp
+print("After swapping:")
+print("First number:", num1)
+print("Second number:", num2)
+
+#without using third variable
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+print("After swapping (without third variable):")
+print("First number:", num1)
+print("Second number:", num2)
